821a055a11529f7698347c9d1ba1e9331e46ad2f
Thimo <68739517+ThimoDEV@users.noreply.github.com>
2023-09-02 00:21:13 +0200

    feat: typescript internal and esvbuild options

--- apps/vike-nitro/nitro.config.ts
+++ apps/vike-nitro/nitro.config.ts
@@ -1,22 +1,36 @@
 import { createServer } from "vite";
-import { defineEventHandler, defineLazyEventHandler, fromNodeMiddleware } from "h3";
+import {
+  defineEventHandler,
+  defineLazyEventHandler,
+  fromNodeMiddleware,
+} from "h3";
 
 //https://nitro.unjs.io/config
 export default defineNitroConfig({
-    srcDir: 'server/',
-    buildDir: 'server/.nitro',
-    devHandlers: [
-        {
-            route: '/__vite',
-            handler: defineLazyEventHandler(async () => {
-                const viteDevServer = await createServer({
-                    base: '/__vite/',
-                    appType: 'custom',
-                    server: { middlewareMode: true },
-                })
+  typescript: { internalPaths: true },
+  esbuild: {
+    options: {
+      loaders: {
+        ".tsx": "tsx",
+      },
+    },
+  },
+  srcDir: "server/",
+  buildDir: "server/.nitro",
+  devHandlers: [
+    {
+      route: "/__vite",
+      handler: defineLazyEventHandler(async () => {
+        const viteDevServer = await createServer({
+          base: "/__vite/",
+          appType: "custom",
+          server: { middlewareMode: true },
+        });
 
-                return defineEventHandler(fromNodeMiddleware(viteDevServer.middlewares))
-            })
-        }
-    ]
+        return defineEventHandler(
+          fromNodeMiddleware(viteDevServer.middlewares),
+        );
+      }),
+    },
+  ],
 });
