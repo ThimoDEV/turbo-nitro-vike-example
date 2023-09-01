d4e3fea59a0435988c5248fca6a6e3f2b87acd1a
Thimo <68739517+ThimoDEV@users.noreply.github.com>
2023-09-02 00:18:55 +0200

    feat: nitro config vite dev server

--- apps/vike-nitro/nitro.config.ts
+++ apps/vike-nitro/nitro.config.ts
@@ -1,5 +1,22 @@
+import { createServer } from "vite";
+import { defineEventHandler, defineLazyEventHandler, fromNodeMiddleware } from "h3";
+
 //https://nitro.unjs.io/config
 export default defineNitroConfig({
     srcDir: 'server/',
     buildDir: 'server/.nitro',
+    devHandlers: [
+        {
+            route: '/__vite',
+            handler: defineLazyEventHandler(async () => {
+                const viteDevServer = await createServer({
+                    base: '/__vite/',
+                    appType: 'custom',
+                    server: { middlewareMode: true },
+                })
+
+                return defineEventHandler(fromNodeMiddleware(viteDevServer.middlewares))
+            })
+        }
+    ]
 });
